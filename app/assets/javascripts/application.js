17c8d8a69f5e88791e2a13614c9740d242e12723
gabrielsuriano <gabriel.suriano@usp.br>
2019-09-05 02:36:37 -0300

    Add menu and sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require turbolinks
 //= require_tree .
 //= require materialize-sprockets
+
+document.addEventListener('DOMContentLoaded', function() {
+	var options = {};
+	var elems = document.querySelectorAll('.sidenav');
+	var instances = M.Sidenav.init(elems, options);
+});
