46c2711acd73b76832a90dbaaea2f48a95b5b2f3
vieira-andre <andre.vieira@outlook.com>
2019-09-09 06:28:56 -0700

    Recuperar info de fundo específico

--- app/scraper.rb
+++ app/scraper.rb
@@ -12,6 +12,12 @@ def scraper
   primeiro_lista = lista_fundos[0]
   nome_fundo = primeiro_lista.children[0].text
 
+  # fundo ABCP11
+  fundo_url = 'https://www.fundsexplorer.com.br/funds/abcp11/'
+  fundo_pag_nao_parseada = HTTParty.get(fundo_url)
+  fundo_pag_parseada = Nokogiri::HTML(fundo_pag_nao_parseada)
+  preco = fundo_pag_parseada.css('span.price')[0].text.tr('\n', '').strip
+
   byebug
 end
 
