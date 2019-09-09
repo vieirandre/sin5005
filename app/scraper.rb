1a602165e69cccd00c93f81efdb56f019fbcc996
vieira-andre <andre.vieira@outlook.com>
2019-09-09 06:28:15 -0700

    Recuperar lista de fundos

--- app/scraper.rb
+++ app/scraper.rb
@@ -3,7 +3,16 @@ require 'nokogiri'
 require 'byebug'
 
 def scraper
+  # lista de fundos
+  url = 'https://www.fundsexplorer.com.br/funds/'
+  pag_nao_parseada = HTTParty.get(url)
+  pag_parseada = Nokogiri::HTML(pag_nao_parseada)
+  lista_fundos = pag_parseada.css('span.symbol')
+  contagem_fundos = lista_fundos.count
+  primeiro_lista = lista_fundos[0]
+  nome_fundo = primeiro_lista.children[0].text
 
+  byebug
 end
 
 scraper
