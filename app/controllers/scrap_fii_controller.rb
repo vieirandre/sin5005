5991aa342e98ba5480040db7cca822e3d690b853
Gabriel Menossi Suriano <gabriel.suriano@usp.br>
2019-09-25 13:24:20 -0300

    O código era fixo, agora ele é pego por parâmetros. Colocado tratativas de erro para: Fundo sem notícias, fundo sem rendimento, e fundo inexistente.

--- app/controllers/scrap_fii_controller.rb
+++ app/controllers/scrap_fii_controller.rb
@@ -4,14 +4,40 @@ class ScrapFiiController < ApplicationController
 
 	def nokogiritest
 		# Fetch and parse HTML document
-		doc = Nokogiri::HTML(open('https://fiis.com.br/knri11'))
-		items = []
-		doc.css('.entry-content ul li').each do |link|
-			if link.content.include? "Informou distribuição de:"
-				items.push(link.content)
+		if(params.has_key?(:codigo))
+			begin
+				puts "O CÓDIGO É: " + params[:codigo]
+				site = "https://fiis.com.br/" + params[:codigo]
+				doc = Nokogiri::HTML(open(site))
+				items = []
+				numeroDeNoticias = 0
+				numeroDeRendimentos = 0
+				doc.css('.entry-content ul li').each do |link|
+					numeroDeNoticias = numeroDeNoticias + 1
+					if link.content.include? "Informou distribuição de:"
+						items.push(link.content)
+						numeroDeRendimentos = numeroDeRendimentos + 1
+					end
+				end
+				if (numeroDeNoticias > 0)
+					if (numeroDeRendimentos > 0)
+						return items
+					else
+						return "Não houve fatos relevantes desse ativo"
+					end
+				else
+					return "Não existem notícias desse ativo. Verifique se ele existe"
+				end
+			rescue OpenURI::HTTPError => e
+				if e.message == '404 Not Found'
+					return "O código '" + params[:codigo] + "' não existe. Verifique e tente novamente"
+				else
+					raise e
+				end
 			end
+		else
+			return "Erro: Informe o código do ativo pelo parâmetro 'codigo'"
 		end
-		return items
 	end
 
 	def scrapFii
