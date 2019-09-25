d3b5d62b739bb35f65b06c41f265c441cb89f23e
Gabriel Menossi Suriano <gabriel.suriano@usp.br>
2019-09-25 07:57:03 -0300

    Add nokogiri to scrapfii

--- app/controllers/scrap_fii_controller.rb
+++ app/controllers/scrap_fii_controller.rb
@@ -1,8 +1,21 @@
 class ScrapFiiController < ApplicationController
 	require 'nokogiri'
 	require 'open-uri'
-	teste = 10
-	puts teste
+
+	def nokogiritest
+		# Fetch and parse HTML document
+		doc = Nokogiri::HTML(open('https://fiis.com.br/knri11'))
+		items = []
+		doc.css('.entry-content ul li').each do |link|
+			if link.content.include? "Informou distribuição de:"
+				items.push(link.content)
+			end
+		end
+		return items
+	end
+
 	def scrapFii
+		@teste = 10
+		@ngiri = nokogiritest
 	end
 end
