b8a54499ceb315d1f8f8d7e77b2faae9e0774f62
Gabriel Menossi Suriano <gabriel.suriano@usp.br>
2019-09-25 21:22:19 -0300

    Função de scrap separada da view, aplicação de regex para separar os valores do fundo, apresentação dos valores por tabela.

--- app/controllers/scrap_fii_controller.rb
+++ app/controllers/scrap_fii_controller.rb
@@ -2,38 +2,89 @@ class ScrapFiiController < ApplicationController
 	require 'nokogiri'
 	require 'open-uri'
 
-	def nokogiritest
+	def pegarValorComRegex(regex1, regex2, texto)
+		resultadoRegex1 = texto.scan(regex1)
+		if (!resultadoRegex1.empty? && !resultadoRegex1.first.empty? && !resultadoRegex1.first.first.empty?)
+			resultadoRegex1 = resultadoRegex1.first.first.to_s
+			resultadoRegex2 = resultadoRegex1.scan(regex2)
+			if (!resultadoRegex2.empty? && !resultadoRegex2.first.empty? && !resultadoRegex2.first.first.empty?)
+				resultado = resultadoRegex2.first.first.to_s
+			else
+				resultado = '-'
+			end
+		else
+			resultado = '-'
+		end
+	end
+
+	def realizarScrapFii(codigo)
 		# Fetch and parse HTML document
+		begin
+			site = "https://fiis.com.br/" + codigo
+			doc = Nokogiri::HTML(open(site))
+			items = []
+			numeroDeNoticias = 0
+			numeroDeRendimentos = 0
+			doc.css('.entry-content ul li').each do |link|
+				numeroDeNoticias = numeroDeNoticias + 1
+				if link.content.include? "Informou distribuição de:"
+					regexMonetario = /(([0-9]*),([0-9]*))/i
+					regexData = /(([0-9]*)\/([0-9]*)\/([0-9]*))/i
+
+					regexRendimento = /(Rendimento no valor de R\$ ([0-9]*),([0-9]*))/i
+					rendimento = pegarValorComRegex(regexRendimento, regexMonetario, link.content)
+
+					regexDiaDaDistribuicao = /(por cota no dia ([0-9]*)\/([0-9]*)\/([0-9]*))/i
+					diaDaDistribuicao = pegarValorComRegex(regexDiaDaDistribuicao, regexData, link.content)
+
+					regexValorAtivoFechamento = /((\s)*Fechamento\:(\s)*R\$(\s)*([0-9]*),([0-9]*))/i
+					valorAtivoFechamento = pegarValorComRegex(regexValorAtivoFechamento, regexMonetario, link.content)
+
+					regexDataBaseFechamento = /(Data base: ([0-9]*)\/([0-9]*)\/([0-9]*))/i
+					dataBaseFechamento = pegarValorComRegex(regexDataBaseFechamento, regexData, link.content)
+					
+					# puts "Rendimento: " + rendimento
+					# puts "diaDaDistribuicao: " + diaDaDistribuicao
+					# puts "valorAtivoFechamento: " +valorAtivoFechamento
+					# puts "dataBaseFechamento: " + dataBaseFechamento
+
+					item = {}
+					item['rendimento'] = rendimento
+					item['diaDaDistribuicao'] = diaDaDistribuicao
+					item['valorAtivoFechamento'] = valorAtivoFechamento
+					item['dataBaseFechamento'] = dataBaseFechamento
+
+
+					items.push(item)
+					numeroDeRendimentos = numeroDeRendimentos + 1
+				end
+			end
+			if (numeroDeNoticias > 0)
+				if (numeroDeRendimentos > 0)
+					return items
+				else
+					return "Não houve fatos relevantes desse ativo"
+				end
+			else
+				return "Não existem notícias desse ativo. Verifique se ele existe"
+			end
+		rescue OpenURI::HTTPError => e
+			if e.message == '404 Not Found'
+				return "O código '" + codigo + "' não existe. Verifique e tente novamente"
+			else
+				raise e
+			end
+		end
+	end
+
+	def pegarCodigoEChamarScrap
 		if(params.has_key?(:codigo))
-			begin
-				puts "O CÓDIGO É: " + params[:codigo]
-				site = "https://fiis.com.br/" + params[:codigo]
-				doc = Nokogiri::HTML(open(site))
-				items = []
-				numeroDeNoticias = 0
-				numeroDeRendimentos = 0
-				doc.css('.entry-content ul li').each do |link|
-					numeroDeNoticias = numeroDeNoticias + 1
-					if link.content.include? "Informou distribuição de:"
-						items.push(link.content)
-						numeroDeRendimentos = numeroDeRendimentos + 1
-					end
-				end
-				if (numeroDeNoticias > 0)
-					if (numeroDeRendimentos > 0)
-						return items
-					else
-						return "Não houve fatos relevantes desse ativo"
-					end
-				else
-					return "Não existem notícias desse ativo. Verifique se ele existe"
-				end
-			rescue OpenURI::HTTPError => e
-				if e.message == '404 Not Found'
-					return "O código '" + params[:codigo] + "' não existe. Verifique e tente novamente"
-				else
-					raise e
-				end
+			codigo = params[:codigo]
+			puts "O CÓDIGO É: " + codigo
+			if (codigo != "")
+				return realizarScrapFii(codigo)
+			else
+				return "Erro: Nenhum código foi informado"
 			end
 		else
 			return "Erro: Informe o código do ativo pelo parâmetro 'codigo'"
@@ -42,6 +93,6 @@ class ScrapFiiController < ApplicationController
 
 	def scrapFii
 		@teste = 10
-		@ngiri = nokogiritest
+		@ngiri = pegarCodigoEChamarScrap
 	end
 end
