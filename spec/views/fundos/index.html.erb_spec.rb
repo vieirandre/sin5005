4ae347d082b261400406f108c313ea058bcbf671
vieira-andre <andre.vieira@outlook.com>
2019-10-12 12:46:38 -0700

    Corrigir spec do index de fundos

--- spec/views/fundos/index.html.erb_spec.rb
+++ spec/views/fundos/index.html.erb_spec.rb
@@ -8,42 +8,42 @@ RSpec.describe "fundos/index", type: :view do
         :nome => "Nome",
         :cnpj => "Cnpj",
         :segmento => "Segmento",
-        :tx_adm => "",
-        :data_const => "Data Const",
-        :num_cotas_emitidas => "",
-        :patrimonio_inicial => "",
-        :valor_inicial_cota => "",
+        :tx_adm => "Tx adm",
+        :data_const => "Data const",
+        :num_cotas_emitidas => "Num cotas emitidas",
+        :patrimonio_inicial => "Patrimonio inicial",
+        :valor_inicial_cota => "Valor inicial cota",
         :prazo => "Prazo",
-        :tipo_gestao => "Tipo Gestao"
+        :tipo_gestao => "Tipo gestao"
       ),
       Fundo.create!(
         :ticker => "Ticker",
         :nome => "Nome",
         :cnpj => "Cnpj",
         :segmento => "Segmento",
-        :tx_adm => "",
-        :data_const => "Data Const",
-        :num_cotas_emitidas => "",
-        :patrimonio_inicial => "",
-        :valor_inicial_cota => "",
+        :tx_adm => "Tx adm",
+        :data_const => "Data const",
+        :num_cotas_emitidas => "Num cotas emitidas",
+        :patrimonio_inicial => "Patrimonio inicial",
+        :valor_inicial_cota => "Valor inicial cota",
         :prazo => "Prazo",
-        :tipo_gestao => "Tipo Gestao"
+        :tipo_gestao => "Tipo gestao"
       )
     ])
   end
 
   it "renders a list of fundos" do
     render
-    assert_select "tr>td", :text => "Ticker".to_s, :count => 2
-    assert_select "tr>td", :text => "Nome".to_s, :count => 2
-    assert_select "tr>td", :text => "Cnpj".to_s, :count => 2
-    assert_select "tr>td", :text => "Segmento".to_s, :count => 2
-    assert_select "tr>td", :text => "".to_s, :count => 2
-    assert_select "tr>td", :text => "Data Const".to_s, :count => 2
-    assert_select "tr>td", :text => "".to_s, :count => 2
-    assert_select "tr>td", :text => "".to_s, :count => 2
-    assert_select "tr>td", :text => "".to_s, :count => 2
-    assert_select "tr>td", :text => "Prazo".to_s, :count => 2
-    assert_select "tr>td", :text => "Tipo Gestao".to_s, :count => 2
+    assert_select "tr>th", :text => "Ticker".to_s, :count => 1
+    assert_select "tr>th", :text => "Nome".to_s, :count => 1
+    assert_select "tr>th", :text => "Cnpj".to_s, :count => 1
+    assert_select "tr>th", :text => "Segmento".to_s, :count => 1
+    assert_select "tr>th", :text => "Tx adm".to_s, :count => 1
+    assert_select "tr>th", :text => "Data const".to_s, :count => 1
+    assert_select "tr>th", :text => "Num cotas emitidas".to_s, :count => 1
+    assert_select "tr>th", :text => "Patrimonio inicial".to_s, :count => 1
+    assert_select "tr>th", :text => "Valor inicial cota".to_s, :count => 1
+    assert_select "tr>th", :text => "Prazo".to_s, :count => 1
+    assert_select "tr>th", :text => "Tipo gestao".to_s, :count => 1
   end
 end
