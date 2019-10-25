a6604263878b091233bef81c3b4bfc0f43d097b1
Gabriel Menossi Suriano <gabriel.suriano@usp.br>
2019-10-25 13:07:00 -0300

    Realização dos testes e termino de refatoração

--- app/models/dados_fundo.rb
+++ app/models/dados_fundo.rb
@@ -39,7 +39,7 @@ class DadosFundo < ApplicationRecord
 	end
 
 	def self.pegarRendimento(conteudo)
-		regexRendimento = /(Rendimento(\s)*no(\s)*valor(\s)*de(\s)*R\$(\s)*(((([0-9]*)\.)*)?([0-9]*),([0-9]*)))/i
+		regexRendimento = /(Rendimento(\s)*no(\s)*valor(\s)*de(\s)*R\$(\s)*(((([0-9]*)\.)*)?([0-9]*)\,([0-9]*)))/i
 		return self.recuperarInformacaoComRegex(regexRendimento, self.regexMonetario, conteudo)
 	end
 
@@ -49,7 +49,7 @@ class DadosFundo < ApplicationRecord
 	end
 
 	def self.pegarValorAtivoDiaDeFechamento(conteudo)
-		regexValorAtivoFechamento = /(Fechamento\:(\s)*R\$(\s)*(((([0-9]*)\.)*)?([0-9]*),([0-9]*)))/i
+		regexValorAtivoFechamento = /(Fechamento\:(\s)*R\$(\s)*(((([0-9]*)\.)*)?([0-9]*)\,([0-9]*)))/i
 		return self.recuperarInformacaoComRegex(regexValorAtivoFechamento, self.regexMonetario, conteudo)
 	end
 
@@ -62,47 +62,69 @@ class DadosFundo < ApplicationRecord
 		conteudo.include? "Informou distribuição de:"
 	end
 
-	def self.gerarItem(conteudo)
-		item = {}
-		item['rendimento'] = self.pegarRendimento(conteudo)
-		item['diaDaDistribuicao'] = self.pegarDiaDaDistribuicao(conteudo)
-		item['valorAtivoFechamento'] = self.pegarValorAtivoDiaDeFechamento(conteudo)
-		item['dataBaseFechamento'] = self.pegarDataBaseFechamento(conteudo)
-		return item
+	def self.gerarItem(conteudo, itens, numeroDeRendimentos)
+		if self.verificarSeExisteAlgumRendimento(conteudo)
+			item = {}
+			item['rendimento'] = self.pegarRendimento(conteudo)
+			item['diaDaDistribuicao'] = self.pegarDiaDaDistribuicao(conteudo)
+			item['valorAtivoFechamento'] = self.pegarValorAtivoDiaDeFechamento(conteudo)
+			item['dataBaseFechamento'] = self.pegarDataBaseFechamento(conteudo)
+			itens.push(item)
+			numeroDeRendimentos = numeroDeRendimentos + 1
+		end
+		return numeroDeRendimentos
+	end
+
+	def self.traduzirErroAbrirSite(erro, codigo)
+		if erro == '404 Not Found'
+			return "O código '" + codigo + "' não existe. Verifique e tente novamente"
+		else
+			return false
+		end
+	end
+
+	def self.abrirSite(codigo)
+		begin
+			site = "https://fiis.com.br/" + codigo
+			doc = Nokogiri::HTML(open(site))
+			return doc, false, false
+		rescue OpenURI::HTTPError => e
+			erro = traduzirErroAbrirSite(e.message, codigo)
+			if !erro
+				return false, e, false
+			else
+				return false, e, erro
+			end
+		end
+	end
+
+	def self.retornoNoticiasInvestimento(numeroDeNoticias, numeroDeRendimentos, itens)
+		if (numeroDeNoticias == 0)
+			return "Não existem notícias desse ativo. Verifique se ele existe"
+		elsif (numeroDeRendimentos == 0)
+			return "Não houve fatos relevantes desse ativo"
+		else
+			return itens
+		end
 	end
 
 	def self.realizarScrapFii(codigo)
 		# Fetch and parse HTML document
-		begin
-			site = "https://fiis.com.br/" + codigo
-			doc = Nokogiri::HTML(open(site))
-			items = []
+		doc, e, erro = self.abrirSite(codigo)
+		if !e
+			itens = []
 			numeroDeNoticias = 0
 			numeroDeRendimentos = 0
 			doc.css('.entry-content ul li').each do |link|
 				numeroDeNoticias = numeroDeNoticias + 1
-				if verificarSeExisteAlgumRendimento(link.content)
-					items.push(self.gerarItem(link.content))
-					numeroDeRendimentos = numeroDeRendimentos + 1
-				end
+				numeroDeRendimentos = self.gerarItem(link.content, itens, numeroDeRendimentos)
 			end
-			if (numeroDeNoticias > 0)
-				if (numeroDeRendimentos > 0)
-					return items
-				else
-					return "Não houve fatos relevantes desse ativo"
-				end
-			else
-				return "Não existem notícias desse ativo. Verifique se ele existe"
-			end
-		rescue OpenURI::HTTPError => e
-			if e.message == '404 Not Found'
-				return "O código '" + codigo + "' não existe. Verifique e tente novamente"
-			else
-				raise e
-			end
+			return self.retornoNoticiasInvestimento(numeroDeNoticias, numeroDeRendimentos, itens)
+		else
+			return erro || e
 		end
 	end
+
 	def self.salvarFundo(codigo, listaResultados)
 		if (listaResultados.kind_of?(Array))
 			listaResultados.each do |item|
@@ -113,6 +135,9 @@ class DadosFundo < ApplicationRecord
 					fundo.precoAtivoNoFechamento = item['valorAtivoFechamento']
 				end
 			end
+			return true
+		else
+			return false
 		end
 	end
-end
+end	
