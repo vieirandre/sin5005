ddcbae2acf1d51494bece42c535fb956d0546c4a
vieira-andre <andre.vieira@outlook.com>
2019-10-25 23:33:12 -0700

    Adicionar rota para action 'popula'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'fundos', to: 'fundos#index'
+  get 'fundos/popula', to: 'fundos#popula'
   get 'fundos/:ticker', to: 'fundos#recupera'
   resources :dados_fundos
   get 'scrap_fii/scrapFii'
