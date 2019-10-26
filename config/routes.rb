838ad03ce47798556cc2ed7bf1f8998cd2097089
vieira-andre <andre.vieira@outlook.com>
2019-10-25 19:07:18 -0700

    Estabelecer rotas para actions 'index' e 'recupera' do controller fundos

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'fundos', to: 'fundos#index'
+  get 'fundos/:ticker', to: 'fundos#recupera'
   resources :dados_fundos
   get 'scrap_fii/scrapFii'
-  resources :fundos
-  root 'home#index'
   get 'noticias/fii/:id', to: "noticias#fii"
   get 'noticias/fii/', to: "noticias#fii"
 end
