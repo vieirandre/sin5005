e975c2bcaaafb7ef60138cf1ae79dde9e3f24ec4
vieira-andre <andre.vieira@outlook.com>
2019-10-25 23:26:35 -0700

    Refatorar método 'scrap' do modelo 'fundo'

--- app/models/fundo.rb
+++ app/models/fundo.rb
@@ -3,7 +3,17 @@ class Fundo < ApplicationRecord
   require 'nokogiri'
 
   def self.scrap(ticker)
-=begin
+    url = "https://www.fundsexplorer.com.br/funds/#{ticker}/"
+    pag_nao_parseada = HTTParty.get(url)
+    pag_parseada = Nokogiri::HTML(pag_nao_parseada)
+    title = pag_parseada.title
+
+    return if title['500'] || title['400'] || title['404']
+
+    fundo = extrair_infos(ticker, pag_parseada)
+
+    salvar(fundo)
+  end
     url = 'https://www.fundsexplorer.com.br/funds/'
     pag_nao_parseada = HTTParty.get(url)
     pag_parseada = Nokogiri::HTML(pag_nao_parseada)
@@ -28,10 +38,30 @@ class Fundo < ApplicationRecord
     prazo = fundo_pag_parseada.css('span.description')[12].text.strip
     tipo_gestao = fundo_pag_parseada.css('span.description')[5].text.strip
 
+  private
+
     Fundo.create(ticker: ticker, preco: preco, nome: nome, cnpj: cnpj, segmento: segmento,
                  tx_adm: tx_adm, data_const: data_const, num_cotas_emitidas: num_cotas_emitidas,
                  patrimonio_inicial: patrimonio_inicial, valor_inicial_cota: valor_inicial_cota,
                  prazo: prazo, tipo_gestao: tipo_gestao)
+  def self.extrair_infos(ticker, pag_parseada)
+    Fundo.new do |f|
+      f.ticker = ticker
+      f.preco = pag_parseada.css('span.price')[0].text.delete('R$').strip
+      f.nome = pag_parseada.css('h2.section-subtitle')[0].text
+      f.cnpj = pag_parseada.css('span.description')[8].text.strip
+      f.segmento = pag_parseada.css('span.description')[11].text.strip
+      f.tx_adm = pag_parseada.css('span.description')[13].text.strip.delete('^0-9.').chomp('.')
+      f.data_const = pag_parseada.css('span.description')[1].text.strip
+      f.num_cotas_emitidas = pag_parseada.css('span.description')[2].text.strip
+      f.patrimonio_inicial = pag_parseada.css('span.description')[3].text.delete('R$').strip
+      f.valor_inicial_cota = pag_parseada.css('span.description')[4].text.delete('R$').strip
+      f.prazo = pag_parseada.css('span.description')[12].text.strip
+      f.tipo_gestao = pag_parseada.css('span.description')[5].text.strip
+    end
   end
 
+  def self.salvar(fundo)
+    fundo.save
+  end
 end
