0455f7f3b366baae1c55fea36639f34fc0253f14
vieira-andre <andre.vieira@outlook.com>
2019-10-25 23:37:12 -0700

    Deletar actions e views obsoletas

--- app/controllers/fundos_controller.rb
+++ app/controllers/fundos_controller.rb
@@ -33,34 +33,4 @@ class FundosController < ApplicationController
     Fundo.popula
     index
   end
-
-  def create
-    @fundo = Fundo.new(fundo_params)
-
-    respond_to do |format|
-      if @fundo.save
-        format.html { redirect_to @fundo, notice: 'Fundo was successfully created.' }
-        format.json { render :show, status: :created, location: @fundo }
-      else
-        format.html { render :new }
-        format.json { render json: @fundo.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @fundo.destroy
-    respond_to do |format|
-      format.html { redirect_to fundos_url, notice: 'Fundo was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-
-  def fundo_params
-    params.require(:fundo).permit(:ticker, :nome, :cnpj, :segmento, :tx_adm,
-                                  :data_const, :num_cotas_emitidas, :patrimonio_inicial,
-                                  :valor_inicial_cota, :prazo, :tipo_gestao)
-  end
 end
