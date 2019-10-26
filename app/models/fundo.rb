157fd7585de086bb3cf5542ec7962110675672cb
vieira-andre <andre.vieira@outlook.com>
2019-10-25 19:53:05 -0700

    Atualizar função de scrap para salvar fundo no banco

--- app/models/fundo.rb
+++ app/models/fundo.rb
@@ -2,7 +2,8 @@ class Fundo < ApplicationRecord
   require 'httparty'
   require 'nokogiri'
 
-  def scraper
+  def self.scrap(ticker)
+=begin
     url = 'https://www.fundsexplorer.com.br/funds/'
     pag_nao_parseada = HTTParty.get(url)
     pag_parseada = Nokogiri::HTML(pag_nao_parseada)
@@ -10,12 +11,13 @@ class Fundo < ApplicationRecord
     contagem_fundos = lista_fundos.count
     primeiro_lista = lista_fundos[0]
     ticker_fundo = primeiro_lista.children[0].text
+=end
 
-    fundo_url = 'https://www.fundsexplorer.com.br/funds/abcp11/'
+    fundo_url = "https://www.fundsexplorer.com.br/funds/#{ticker}/"
     fundo_pag_nao_parseada = HTTParty.get(fundo_url)
     fundo_pag_parseada = Nokogiri::HTML(fundo_pag_nao_parseada)
     preco = fundo_pag_parseada.css('span.price')[0].text.delete('R$').strip
-    nome_fundo = fundo_pag_parseada.css('h2.section-subtitle')[0].text
+    nome = fundo_pag_parseada.css('h2.section-subtitle')[0].text
     cnpj = fundo_pag_parseada.css('span.description')[8].text.strip
     segmento = fundo_pag_parseada.css('span.description')[11].text.strip
     tx_adm = fundo_pag_parseada.css('span.description')[13].text.strip.delete('^0-9.').chomp('.')
@@ -25,5 +27,11 @@ class Fundo < ApplicationRecord
     valor_inicial_cota = fundo_pag_parseada.css('span.description')[4].text.delete('R$').strip
     prazo = fundo_pag_parseada.css('span.description')[12].text.strip
     tipo_gestao = fundo_pag_parseada.css('span.description')[5].text.strip
+
+    Fundo.create(ticker: ticker, preco: preco, nome: nome, cnpj: cnpj, segmento: segmento,
+                 tx_adm: tx_adm, data_const: data_const, num_cotas_emitidas: num_cotas_emitidas,
+                 patrimonio_inicial: patrimonio_inicial, valor_inicial_cota: valor_inicial_cota,
+                 prazo: prazo, tipo_gestao: tipo_gestao)
   end
+
 end
