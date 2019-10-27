07dff12cd6476a87c6a9b4441db2dc6c054b2c09
vieira-andre <andre.vieira@outlook.com>
2019-10-26 22:09:43 -0700

    Criar testes funcionais para action 'recupera'

--- spec/controllers/fundos_controller_spec.rb
+++ spec/controllers/fundos_controller_spec.rb
@@ -2,8 +2,18 @@ require 'rails_helper'
 
 RSpec.describe FundosController, type: :controller do
 
-  let(:valid_attributes) {}
+  let(:valid_ticker) { 'abcp11' }
+  let(:invalid_ticker) { 'xx11' }
 
-  let(:invalid_attributes) {}
+  describe 'GET #recupera' do
+    it 'retorna uma resposta de sucesso' do
+      get :recupera, params: { ticker: valid_ticker }
+      expect(response).to be_successful
+    end
+    it 'retorna uma resposta de erro' do
+      get :recupera, params: { ticker: invalid_ticker }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
 
 end
