7d21f9414c7c1ccb1d891fa04114e58f100e0847
Gabriel Menossi Suriano <gabriel.suriano@usp.br>
2019-10-27 22:14:36 -0300

    Bugfix - Rotas de arquivos excluidos ainda existiam

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'fundos/:ticker', to: 'fundos#recupera'
   post 'fundos/popula', to: 'fundos#popula'
   resources :dados_fundos
-  get 'scrap_fii/scrapFii'
   get 'noticias/fii/:id', to: "noticias#fii"
   get 'noticias/fii/', to: "noticias#fii"
 end
