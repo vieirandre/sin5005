cee388cd026d495738c3b506e12f6b180a815673
sanchezbruno <sanchez.bruno@hotmail.com>
2019-11-05 17:32:00 -0300

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -24,7 +24,24 @@ ActiveRecord::Schema.define(version: 2019_11_05_195509) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
+  
+  create_table "fundos", force: :cascade do |t|
+    t.string "ticker"
+    t.string "preco"
+    t.string "nome"
+    t.string "cnpj"
+    t.string "segmento"
+    t.string "tx_adm"
+    t.string "data_const"
+    t.string "num_cotas_emitidas"
+    t.string "patrimonio_inicial"
+    t.string "valor_inicial_cota"
+    t.string "prazo"
+    t.string "tipo_gestao"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+  
   create_table "usuarios", force: :cascade do |t|
     t.string "login"
     t.string "senha"
