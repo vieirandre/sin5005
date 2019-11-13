e58147717aba0bf9b09afd6ea7c84025008cb85d
vieira-andre <andre.vieira@outlook.com>
2019-11-13 01:13:04 -0300

    Eliminar migrations

--- db/migrate/20191107062623_remove_preco_ativo_no_fechamento_from_dados_fundos.rb
+++
@@ -1,5 +0,0 @@
-class RemovePrecoAtivoNoFechamentoFromDadosFundos < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :dados_fundos, :precoAtivoNoFechamento, :string
-  end
-end
--- db/migrate/20191107063222_remove_dia_distribuicao_from_dados_fundos.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDiaDistribuicaoFromDadosFundos < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :dados_fundos, :diaDistribuicao, :string
-  end
-end
